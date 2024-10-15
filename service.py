175f1e16858a8f793e87b0b5bd90dcdee8c72312
Laszlo Kiss <laszlo.kiss27@gmail.com>
2024-10-15 13:52:35 +0200

    Fix

--- service.py
+++ service.py
@@ -313,11 +313,3 @@ class CharacteristicUserDescriptionDescriptor(Descriptor):
         if not self.writable:
             raise NotPermittedException()
         self.value = value
-
-    @dbus.service.method(GATT_DESC_IFACE)
-    def StartNotify(self):
-        print('Device connected')
-
-    @dbus.service.method(GATT_DESC_IFACE)
-    def StopNotify(self):
-        print('Device disconnected')
